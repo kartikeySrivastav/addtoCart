7bb4b70a4cec79a20adc0de2b151528410d917c1
kartikey kumar Srivastava <kartikey1425@gmail.com>
2023-07-03 16:27:10 +0530

    adding waste

--- src/components/Header.js
+++ src/components/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
     <>
       <Navbar bg="dark" variant="dark" style={{ height: 60 }}>
         <Container>
-          <NavLink to="/" className=" text-decoration-none text-light mx-3 ">
+          <NavLink className=" text-decoration-none text-light mx-3 ">
             Add to Cart
           </NavLink>
           <Badge
